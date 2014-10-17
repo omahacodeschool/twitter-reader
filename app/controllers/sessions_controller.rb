2b2a4ddb88fbca26b06248bc9499237f36cee67d
“Alyssa <lasher1010@hotmail.com>
2014-10-17 15:02:05 -0500

    Fixed bug where it only showed a certain user's links....hopefully

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,18 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
+    
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["CONSUMER_KEY"]
+      config.consumer_secret     = ENV["CONSUMER_SECRET"]
+      config.access_token        = auth[:credentials][:token]
+      config.access_token_secret = auth[:credentials][:secret]
+    end
+    
+    $tweet_links = client.home_timeline
+    
+    binding.pry
+    
     redirect_to root_url, :notice => "Signed in!"
   end
 
